893e09884a2738d26a468923b729edf979c3007f
MitchellWT <mitchell444999@gmail.com>
2021-04-25 21:04:52 +1000

    Update timezone...

--- config/app.php
+++ config/app.php
@@ -67,7 +67,7 @@ return [
     |
     */
 
-    'timezone' => 'Melbourne',
+    'timezone' => 'Australia/Melbourne',
 
     /*
     |--------------------------------------------------------------------------
